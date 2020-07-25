a7f8b078e853cf63e32493eaafcdcc8b1d3e7156
Oleksii Skopiuk <aztek2008@gmail.com>
2020-07-25 21:37:11 +0300

    Updates

---
+++ precache-manifest.533e1d96a7dab566c15cd0454fe706f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2293a952e38ec89654e00ee73fa283",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "449274e94b8de9b1d75e",
+    "url": "/goit-react-hw-01-components/static/css/main.ce79726b.chunk.css"
+  },
+  {
+    "revision": "d7b5efed7bb164a3f647",
+    "url": "/goit-react-hw-01-components/static/js/2.fcf625f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.fcf625f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "449274e94b8de9b1d75e",
+    "url": "/goit-react-hw-01-components/static/js/main.4254fccb.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
